068e9fb66b4fc6a7b5e3034d2465ac8a1273284e
RafidN <rafidn@outlook.com>
2023-08-06 22:15:05 -0500

    bookInfo and started cart screen

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
+import Book from "../components/ui/Book";
 
-function BookInfo({ books }) {
+function BookInfo({ books, addToCart, cart }) {
+  // Gain access to the book being used using the useParams built in function
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find(book => book.id === +id);
+  }
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -21,22 +34,19 @@ function BookInfo({ books }) {
             <div>
               <div className="book__selected">
                 <figure className="book__selected--figure">
-                  <img
-                    src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
-                    alt=""
-                    className="book__selected--img"
-                  />
+                  <img src={book.url} alt="" className="book__selected--img" />
                 </figure>
                 <div className="book__selected--description">
-                  <h2 className="book__selected--title">
-                    Crack the coding interview
-                  </h2>
-                  <Rating rating="4.5" />
+                  <h2 className="book__selected--title">{book.title}</h2>
+                  <Rating rating={book.rating} />
                   <div className="book__selected--price">
-                    <Price oPrice={50} sPrice={19.99} />
+                    <Price
+                      oPrice={book.originalPrice}
+                      sPrice={book.salePrice}
+                    />
                   </div>
                   <div className="book__summary">
-                    <div className="book__summary--title">Summary</div>
+                    <h3 className="book__summary--title">Summary</h3>
                     <p className="book__summary--para">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Unde omnis, dicta numquam quisquam ipsa aut suscipit
@@ -50,7 +60,15 @@ function BookInfo({ books }) {
                       quasi at optio eligendi totam odit quam?
                     </p>
                   </div>
-                  <button className="btn">Add to Cart</button>
+                  {bookExistsOnCart() ? (
+                  <Link to="/cart" className="book__link">
+                    <button className="btn">Checkout</button>
+                  </Link>
+                  ) : (
+                    <button className="btn" onClick={() => addBookToCart(book)}>
+                      Add to Cart
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
@@ -58,15 +76,23 @@ function BookInfo({ books }) {
         </div>
 
         <div className="books__container">
-            <div className="row">
-                <div className="book__selected--top">
-                    <h2 className="book__selected--title--top">
-                        Recommended Books
-                    </h2>
-                </div>
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
             </div>
+            <div className="books">
+              {
+                books
+                  .filter((book) => book.rating === 5 && +book.id !== +id)
+                  .slice(0, 4)
+                  .map((book) => (
+                    <Book book={book} key={book.id} />
+                  ))
+                // filters books to recommend to be 5 star rated
+              }
+            </div>
+          </div>
         </div>
-
       </main>
     </div>
   );
