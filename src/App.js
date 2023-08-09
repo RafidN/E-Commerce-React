a90580d649b8bae5efa9548c2a59b6b2daefd55a
RafidN <rafidn@outlook.com>
2023-08-08 21:01:13 -0500

    final product

--- src/App.js
+++ src/App.js
@@ -5,15 +5,66 @@ import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => {
+        if (item.id === book.id) {
+          return {
+            ...item,
+            quantity: +quantity,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()}/>
+        <Route exact path="/E-Commerce-React" component={Home} />
         <Route exact path="/" component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route exact path="/books" render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} addToCart={addToCart} cart={cart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem} />
+          )}
+        />
         <Footer />
       </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt)
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft, faSpinner)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
